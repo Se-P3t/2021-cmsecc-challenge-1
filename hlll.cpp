c43944aab596f7842be2bcb60e6ea8d289cf3334
Se-P3t <sep3t.251124@gmail.com>
2021-05-08 20:37:22 +0800

    update

---
+++ hlll.cpp
@@ -0,0 +1,104 @@
+#include <cxxopts.hpp>
+#include <hplll/hlll.h>
+
+using namespace hplll;
+
+
+
+int main(int argc, char *argv[])
+{
+    cxxopts::Options options(argv[0], "Run hlll for a given matrix");
+
+    options.add_options()
+        ("matrix", "input matrix file", cxxopts::value<std::string>())
+        ("out", "file to output lll-reduced matrix (overwrite the input file "
+          "when not specified)", cxxopts::value<std::string>())
+        ("threads", "threads", cxxopts::value<int>()->default_value("1"))
+        ("seysen", "use Seysen reduction", cxxopts::value<bool>()
+          ->implicit_value("true")->default_value("false"))
+
+        ("precision", "bit precision to use",
+          cxxopts::value<unsigned int>()->default_value("53"))
+
+        ("delta", "LLL: delta", cxxopts::value<double>()
+          ->default_value("0.999"))
+        ("threshold", "block size threshold for parallelism", cxxopts::value<int>()
+          ->default_value("1000000")) // no threads by default
+
+        ("verbose", "print verbose info.", cxxopts::value<bool>()
+          ->implicit_value("true")->default_value("false"))
+
+        ("h,help", "Print usage")
+    ;
+    options.parse_positional({"matrix",});
+    options.positional_help("<matrix>");
+    //options.show_positional_help();
+
+    auto args = options.parse(argc, argv);
+
+    if (args.count("help")) {
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
+    bool verbose = args["verbose"].as<bool>();
+    std::string input_file = args["matrix"].as<std::string>();
+    std::string output_file = input_file;
+    if (args.count("out")) {
+        output_file = args["out"].as<std::string>();;
+    }
+    int threads = args["threads"].as<int>();
+    bool seysen = args["seysen"].as<bool>();
+
+    unsigned int precision = args["precision"].as<unsigned int>();
+
+    double delta = args["delta"].as<double>();
+    int threshold = args["threshold"].as<int>();
+
+
+    filebuf fb;
+    iostream os(&fb);
+    ofstream outfile;
+
+    typedef mpz_t integer_t;
+    typedef matrix<Z_NR<integer_t> > MatrixZT;
+    typedef matrix<FP_NR<dpe_t> > MatrixFT;
+    //typedef MatrixPE<double, dpe_t> MatrixFT;
+
+    ZZ_mat<integer_t> A;
+    ZZ_mat<integer_t> AT;
+
+    fb.open(input_file, ios::in);
+    os >> A;
+    fb.close();
+    if (verbose) {
+        cout << "Loaded matrix file: "+input_file+" :: " <<  A.get_rows() << " x "
+        << A.get_cols() << endl;
+    }
+    if (threads > 1 && threshold >= A.get_rows()) {
+        cout << "Warning: the threshold is so big that there is no parallelism" << endl;
+    }
+
+    AT.resize(A.get_cols(), A.get_rows());
+    transpose(AT, A);
+    Lattice<integer_t, dpe_t, MatrixZT, MatrixFT> B(AT, NO_TRANSFORM,
+      seysen ? SEYSEN_REDUCTION : DEF_REDUCTION);
+    B.set_num_S(threads, threshold);
+    B.setprec(precision);
+
+    B.hlll(delta, verbose);
+    transpose(A, B.getbase());
+
+    outfile.open(output_file, ios_base::trunc);
+    outfile << A;
+    outfile.close();
+
+    return 0;
+}
+
+
+
+/*
+
+g++ -O3 -Wall -pthread -fopenmp hlll.cpp -o hlll -I/usr/local/include/hplll -I/usr/local/include -L/usr/local/lib -lmpfr -lgmp -lquadmath /usr/local/lib/libfplll.so -lntl
+
+*/
---
+++ hlll.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+hlll wrapper
+"""
+import os
+import subprocess
+
+from fpylll import IntegerMatrix
+
+from util import str_mat
+
+
+def hlll_wrapper(A, threads=4, threshold=40, **kwds):
+    r"""
+    Run hlll for a given matrix
+
+    :param A: an `IntegerMatrix` or `list`
+    :param threads: ... (default: 4)
+    :param threshold: block size threshold for parallelism (default: 40)
+    :param seysen: use Seysen reduction (default: False)
+    :param precision: bit precision to use (default: 53)
+    :param delat: LLL parameter delta, 0 < \delta < 1 (default: 0.999)
+    :param verbose: print verbose info (default: False)
+    """
+    if not os.path.exists("hlll"):
+        raise RuntimeError("cannot find `hlll`")
+
+    if isinstance(A, list):
+        return_list = True
+        m, n = len(A), len(A[0])
+    elif isinstance(A, IntegerMatrix):
+        return_list = False
+        m, n = A.nrows, A.ncols
+    else:
+        raise TypeError("Matrix `A` type '%s' unknown." % type(A))
+    tmpfile = f"hlll_tmp_matrix-{m}-{n}.mat"
+
+    command = f"./hlll {tmpfile} "
+    command += f"--threads {threads} "
+    command += f"--precision {kwds.pop('precision', 53)} "
+    command += f"--delta {kwds.pop('delts', 0.999)} "
+    if kwds.pop('seysen', False):
+        command += "--seysen "
+    if kwds.pop('verbose', False):
+        command += "--verbose "
+
+    with open(tmpfile, "w") as f:
+        f.write(f"[{str_mat(A)}]")
+
+    try:
+        subprocess.check_call(command, shell=True)
+        A = IntegerMatrix.from_file(tmpfile)
+    finally:
+        os.remove(tmpfile)
+
+    if return_list:
+        A = [[A[i, j] for j in range(A.ncols)] for i in range(A.nrows)]
+    return A
