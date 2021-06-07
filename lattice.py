c27df02d23f20a4be2028c626beec9b648e3675b
Se-P3t <sep3t.251124@gmail.com>
2021-06-07 19:21:17 +0800

    use modified 'workout'

--- lattice.py
+++ lattice.py
@@ -2,13 +2,16 @@
 """
 a Lattice object with reduction algorithms
 """
+from __future__ import absolute_import
+from __future__ import print_function
 import sys
+import time
 import random
 
 from fpylll import IntegerMatrix, LLL, BKZ
-from fpylll.util import gaussian_heuristic
+from fpylll.util import gaussian_heuristic, ReductionError
 
-from g6k.algorithms.workout import workout
+from g6k.algorithms.pump import pump
 from g6k.siever import Siever
 from g6k import SieverParams
 from g6k.utils.cli import pop_prefixed_params
@@ -17,6 +20,82 @@ from g6k.utils.util import db_stats
 
 import six
 from six.moves import range
+
+
+def workout(g6k, tracer, kappa, blocksize, dim4free_min=0,              # Main parameters
+            dim4free_dec=2, start_n=40, goal_r0=0.,                     # Loop control
+            verbose=False, save_prefix=None, pump_params=None,          # Misc
+            timeout=None                                                # additional parameter
+            ):
+    """
+    :param g6k: The g6k object to work with
+    :param tracer: A tracer for g6k
+    :param kappa: beginning of the block
+    :param blocksize: dimension of the block
+    :param dim4free_min: Minimal number of dimension for free ``dimension for free'' [Ducas,
+        Eurcrypt 2018] (may stop before reaching that if goal_r0)
+    :param dim4free_dec: By how much do we decreaseee dim4free at each iteration
+    :param start_n: Dimension of the first pump
+    :param goal_r0: an extra hook to always insert at position kappa if this goal length can be met
+        by a lift.  Quit when this is reached.
+    :param verbose: Print workout steps (with timing and quality) information on the standard
+        output.  Enforce verbosity of pump as well.
+    :param save_prefix: If not None, save intermediate basis at a file-name with this prefix.
+        Allows to resume computation.
+    :param pump_params: Parameters to forward to the pump.
+    """
+    if pump_params is None:
+        pump_params = {}
+
+    f_start = max(blocksize - start_n, 0, dim4free_min)
+    fs = list(range(dim4free_min, f_start+1, dim4free_dec))[::-1]
+
+    if goal_r0:
+        fs += 9999*[dim4free_min]
+
+    gh = gaussian_heuristic([g6k.M.get_r(i, i) for i in range(kappa, kappa+blocksize)])
+    runtimestart = time.time()
+
+    if "verbose" not in pump_params:
+        pump_params["verbose"] = verbose
+
+    with tracer.context(("workout", "beta:%d f:%d" % (blocksize, dim4free_min))):
+        for f in fs:
+            flast = f
+            timestart = time.time()
+
+            sys.stdout.flush()
+            try:
+                pump(g6k, tracer, kappa, blocksize, f, goal_r0=goal_r0, **pump_params)
+            except ReductionError as e:
+                if verbose:
+                    print(e)
+                break
+
+            if verbose:
+                r0 = g6k.M.get_r(kappa, kappa)
+                print("T:%10.5fs, TT:%10.5fs, r0:%10.5e r0/gh:%10.5f" %
+                      (time.time() - timestart,
+                       time.time() - runtimestart, r0, r0 / gh))
+
+            if g6k.M.get_r(kappa, kappa) < goal_r0:
+                if save_prefix is not None:
+                    fn = open("%s_%d_%d.sol" % (save_prefix.rstrip(), g6k.M.d - f, g6k.M.d), "w")
+                    fn.write(str(g6k.M.B))
+                    fn.close()
+                break
+
+            if save_prefix is not None:
+                fn = open("%s_%d_%d.mat" % (save_prefix.rstrip(), g6k.M.d - f, g6k.M.d), "w")
+                fn.write(str(g6k.M.B))
+                fn.close()
+
+            if timeout is not None and time.time() - runtimestart > timeout:
+                if verbose:
+                    print('\ntimeout, break')
+                break
+
+    return flast
 
 
 class Lattice(IntegerMatrix):
@@ -161,7 +240,8 @@ class Lattice(IntegerMatrix):
             avg_max, _ = db_stats(stats)
             print(
                 f"{stats.label} :: "
-                f"cputime {data['cputime'].sum:7.4f}s, "
+                f"threads: {threads}, "
+                f"cputime: {data['cputime'].sum:7.4f}s, "
                 f"walltime: {data['walltime'].sum:7.4f}s, "
                 f"flast: {data['flast']:3.2f}, "
                 f"|db|: 2^{avg_max:.2f}"
